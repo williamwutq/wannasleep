aac88b8a528fbbc580a1efabf4da28605bf9a7d7
williamwutq <williamwutq@gmail.com>
2025-12-20 14:53:20 -0800

    more huid

--- src/root.zig
+++ src/root.zig
@@ -21,6 +21,8 @@ pub const HUID = struct {
     /// Initialize a new HUID from the given unix time.
     ///
     /// Note: to obtain the unix time, use std.time.milliTimestamp() / 1000
+    ///
+    /// Must be deinitialized with deinit() to free the allocated string or altered with add()/sub()
     pub fn initid(
         unix_time: i64,
         alloc: std.mem.Allocator,
@@ -48,6 +50,10 @@ pub const HUID = struct {
         };
     }
     /// From the string representation of the HUID, get the ID string
+    ///
+    /// The string is copied into allocated memory.
+    ///
+    /// Must be deinitialized with deinit() to free the allocated string or altered with add()/sub()
     pub fn initstr(
         id_str: []const u8,
         alloc: std.mem.Allocator,
@@ -68,7 +74,7 @@ pub const HUID = struct {
     }
     /// Parse a HUID from a string representation.
     /// Returns an error if the string is invalid.
-    fn parse(id_str: []const u8) !i64 {
+    pub fn parse(id_str: []const u8) !i64 {
         // Expect length
         if (id_str.len != 15) {
             return error.InvalidHUIDString;
@@ -161,6 +167,35 @@ pub const HUID = struct {
     fn isnumber(char: u8) bool {
         return char >= '0' and char <= '9';
     }
+    pub fn compare(self: HUID, other: HUID) i2 {
+        // Directly compare ints
+        if (self.unix_time < other.unix_time) {
+            return -1;
+        } else if (self.unix_time > other.unix_time) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+    /// INCLUSIVE on both ends
+    pub fn inrange(self: HUID, start: HUID, end: HUID) bool {
+        return self.unix_time >= start.unix_time and self.unix_time <= end.unix_time;
+    }
+    // Not an iterator, not named "next"
+    pub fn inc(self: HUID) !HUID {
+        // TODO: This is a bit dumb
+        return HUID.initid(self.unix_time + 1, self.allocator);
+    }
+    // Add seconds to the HUID, returning a new HUID. The old HUID is deinitialized.
+    pub fn add(self: HUID, seconds: i64) !HUID {
+        defer self.deinit();
+        return HUID.initid(self.unix_time + seconds, self.allocator);
+    }
+    // Subtract seconds from the HUID, returning a new HUID. The old HUID is deinitialized.
+    pub fn sub(self: HUID, seconds: i64) !HUID {
+        defer self.deinit();
+        return HUID.initid(self.unix_time - seconds, self.allocator);
+    }
 };
 
 test "Test huid" {
@@ -201,4 +236,51 @@ test "Test invalid huid" {
         const result = HUID.initstr(id_str, allocator);
         try std.testing.expect(result == error.InvalidHUIDString);
     }
+}
+
+test "HUID Compare" {
+    const allocator = std.testing.allocator;
+    const huid1 = try HUID.initid(1625072400, allocator);
+    defer huid1.deinit();
+    const huid2 = try HUID.initid(1625076000, allocator);
+    defer huid2.deinit();
+    const huid3 = try HUID.initid(1625072400, allocator);
+    defer huid3.deinit();
+    try std.testing.expect(huid1.compare(huid2) == -1);
+    try std.testing.expect(huid2.compare(huid1) == 1);
+    try std.testing.expect(huid1.compare(huid3) == 0);
+}
+
+test "HUID InRange" {
+    const allocator = std.testing.allocator;
+    const huid = try HUID.initid(1625072400, allocator);
+    defer huid.deinit();
+    const start = try HUID.initid(1625068800, allocator);
+    defer start.deinit();
+    const end = try HUID.initid(1625076000, allocator);
+    defer end.deinit();
+    try std.testing.expect(huid.inrange(start, end));
+    const before = try HUID.initid(1625065200, allocator);
+    defer before.deinit();
+    try std.testing.expect(!before.inrange(start, end));
+    const after = try HUID.initid(1625079600, allocator);
+    defer after.deinit();
+    try std.testing.expect(!after.inrange(start, end));
+}
+test "HUID Inc" {
+    const allocator = std.testing.allocator;
+    const huid = try HUID.initid(1625072400, allocator);
+    defer huid.deinit();
+    const next_huid = try huid.inc();
+    defer next_huid.deinit();
+    try std.testing.expectEqual(huid.unix_time + 1, next_huid.unix_time);
+}
+test "HUID Add/Sub" {
+    const allocator = std.testing.allocator;
+    const huid = try HUID.initid(1625072400, allocator);
+    const added_huid = try huid.add(3600);
+    try std.testing.expectEqual(huid.unix_time + 3600, added_huid.unix_time);
+    const subbed_huid = try added_huid.sub(3600);
+    defer subbed_huid.deinit();
+    try std.testing.expectEqual(huid.unix_time, subbed_huid.unix_time);
 }
