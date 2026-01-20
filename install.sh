9ec428c4af237094e4d00448ce33d99e3520e290
williamwutq <williamwutq@gmail.com>
2026-01-19 22:38:10 -0800

    minor fix to install script

--- install.sh
+++ install.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-WEB_PATH="https://raw.githubusercontent.com/williamwutq/wannasleep/main"
 RECENT_BUILD="0.1.2-nightly-2026-01-15-debug"
 
 # Auto-update this script
 curl -s "$WEB_PATH/install.sh" -o /tmp/install.sh.new
-if ! cmp -s /tmp/install.sh.new "$0"; then
+SCRIPT_PATH="$(realpath "${BASH_SOURCE[0]}")"
+if ! cmp -s /tmp/install.sh.new "$SCRIPT_PATH"; then
     echo "A new version of the install script is available. Updating..."
-    mv /tmp/install.sh.new "$0"
-    chmod +x "$0"
+    mv /tmp/install.sh.new "$SCRIPT_PATH"
+    chmod +x "$SCRIPT_PATH"
     echo "Restarting the installation with the updated script..."
-    exec "$0"
+    exec "$SCRIPT_PATH"
     exit 0
 else
     rm /tmp/install.sh.new
@@ -42,6 +42,7 @@ case "$ARCH" in
 		;;
 esac
 
+WEB_PATH="https://raw.githubusercontent.com/williamwutq/wannasleep/main"
 FILE_NAME="$PREFIX$RECENT_BUILD"
 DOWNLOAD_URL="$WEB_PATH/builds/$FILE_NAME"
 INSTALL_PATH="/usr/local/bin/todo"
